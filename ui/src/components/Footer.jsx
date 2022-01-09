2abf8abfd97bdba02aa34fbc6f6be728e2ba7026
Matsunaga Takuji <matun-t@sgnet.co.jp>
2022-01-10 06:27:05 +0900

    Fix for safari.

--- ui/src/components/Footer.jsx
+++ ui/src/components/Footer.jsx
@@ -5,7 +5,7 @@ import {faTwitterSquare, faGithub} from "@fortawesome/free-brands-svg-icons";
 function Footer() {
     return (
         <>
-            <Element display="flex" justifyContent="right" my={5}>
+            <Element display="flex" justifyContent="flex-end" my={5}>
                 <Element renderAs="a" display="flex" alignItems="center" mx={3}
                          href="https://twitter.com/sarlos_cainz">
                     <FontAwesomeIcon icon={faTwitterSquare} color="#aaa"/>
